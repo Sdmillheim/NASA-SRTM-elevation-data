82c106d0fc22edbed25214ba828092ebcefa7f12
Stephen <Sdmillheim@users.noreply.github.com>
2016-01-26 09:05:07 -0800

    Update srtmdownload.py

--- srtmdownload.py
+++ srtmdownload.py
@@ -5,7 +5,7 @@ import zipfile
 class srtmDownloader(object):
     def downloadFileL1(self,target):
         file = urllib.request.urlretrieve("http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL1.003/2000.02.11/"+target+".SRTMGL1.hgt.zip", "file.hgt.zip")
-        self.srtmfile = zipfile.ZipFile('file.hgt.zip').extractall()
+        zipfile.ZipFile('file.hgt.zip').extractall()
     def downloadFileL3(self,target):
         file = urllib.request.urlretrieve("http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL3.003/2000.02.11/"+target+".SRTMGL3.hgt.zip", "file.hgt.zip")
-        self.srtmfile = zipfile.ZipFile('file.hgt.zip').extractall()
+        zipfile.ZipFile('file.hgt.zip').extractall()
