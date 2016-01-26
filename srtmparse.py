efecc424b3cb976197ac6de90729f1d7dea4f256
Stephen <Sdmillheim@users.noreply.github.com>
2016-01-25 22:08:25 -0800

    Update srtmparse.py

--- srtmparse.py
+++ srtmparse.py
@@ -3,16 +3,16 @@ import struct
 #define a class with functions to parse HGT files (the srtmParser class is adapted from a stack-exchange post)
 class srtmParser(object):
     def parseFileL1(self,filename):
-        # read 1,442,401 (1201x1201) high-endian signed 16-bit words into self.z
-        # used for Level 1 SRTM data (3-arc second granularity)
-        fi=open(filename,"rb")
-        contents=fi.read()
-        fi.close()
-        self.hgtcontents=struct.unpack(">1442401H", contents)
-    def parseFileL3(self,filename):
         # read 12,967,201 (3601x3601) high-endian signed 16-bit words into self.z
         # used for Level 3 SRTM data (1-arc second granularity)
         fi=open(filename,"rb")
         contents=fi.read()
         fi.close()
         self.hgtcontents=struct.unpack(">12967201H", contents)
+    def parseFileL3(self,filename):
+        # read 1,442,401 (1201x1201) high-endian signed 16-bit words into self.z
+        # used for Level 1 SRTM data (3-arc second granularity)
+        fi=open(filename,"rb")
+        contents=fi.read()
+        fi.close()
+        self.hgtcontents=struct.unpack(">1442401H", contents)
