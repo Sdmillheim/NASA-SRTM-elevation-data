73d132d8713df86068b7c727df46cb859bbc4635
Stephen <Sdmillheim@users.noreply.github.com>
2016-01-25 21:12:23 -0800

    Create srtmdownload.py
    define class with functions to download srtm files

---
+++ srtmdownload.py
@@ -0,0 +1,11 @@
+#define a class with functions to download zipped file and extract contents as hgt file named 'target' + .hgt
+import urllib
+import zipfile
+
+class srtmDownloader(object):
+    def downloadFileL1(self,target):
+        file = urllib.request.urlretrieve("http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL1.003/2000.02.11/"+target+".SRTMGL1.hgt.zip", "file.hgt.zip")
+        self.srtmfile = zipfile.ZipFile('file.hgt.zip').extractall()
+    def downloadFileL3(self,target):
+        file = urllib.request.urlretrieve("http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL3.003/2000.02.11/"+target+".SRTMGL3.hgt.zip", "file.hgt.zip")
+        self.srtmfile = zipfile.ZipFile('file.hgt.zip').extractall()
