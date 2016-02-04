496b440c4998a1c5dd89e9287deef33fb8b1cc3c
Stephen <Sdmillheim@users.noreply.github.com>
2016-02-04 14:30:28 -0800

    Create srtmchoose.py

---
+++ srtmchoose.py
@@ -0,0 +1,29 @@
+# Define a class with functions to select parameters for data download and format
+import numpy as np
+
+class srtmChoose(object):
+    def chooseTarget(self):
+        target = '________'
+        while not (target[0] in ("N", "E") and target[1:3].isdigit() and target[3] in ("W", "E") and target[4:7].isdigit()):
+            print('Select target coordinates (Format example: N46W122)')
+            target = input('--> ')
+        return target
+    def chooseDetail(self):
+        detail = ''
+        while detail != '1' and detail != '3':
+            print('Select level of detail. Enter 1 for 1-arc second (higher detail) or 3 for 3-arc second (lower detail)')
+            detail = input('--> ')
+        csvtarget = np.zeros(((3600/int(detail))**2,3)) #either 3600X3600 or 1200X1200. One less than file dimension because files overlap. 
+        width = int(3600/int(detail) + 1)
+        horizontalscale = 30*int(detail)
+        return (detail, csvtarget, width, horizontalscale)
+    def chooseUnits(self):
+        units = ''
+        while units not in ("Feet", "Meters"):
+            print("please input either 'Feet' or 'Meters'")
+            units = input('--> ')
+        if units == "Feet":
+            unitscale = 3.281 # this is feet per 1 meter
+        else:
+            unitscale = 1
+        return unitscale
