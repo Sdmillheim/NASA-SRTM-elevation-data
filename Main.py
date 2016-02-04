b22b2d7b0cc14246ec79382eb8514132ae4ead37
Stephen <Sdmillheim@users.noreply.github.com>
2016-02-04 14:29:53 -0800

    Update Main.py
    Placed parameter selection into its own class.

--- Main.py
+++ Main.py
@@ -1,47 +1,13 @@
 import numpy as np
+import srtmchoose
 import srtmdownload
 import srtmparse
 
-# Select target coordinates, level of detail, and units of altitude
-while True:
-    try:
-        target = input("Select target coordinates (example: N46W122)")
-    except ValueError:
-        print("invalid target format")
-        continue
-    if target[0] in ("N", "E") and target[1:3].isdigit() and target[3] in ("W", "E") and target[4:7].isdigit():
-        break
-    else:
-        print("Please enter coordinates in the correct format. Examples include: N46W122, N27E088, or S32W071")
-while True:
-    try:
-        detail = int(input("Select level of detail. Enter 1 for 1-arc second (higher detail) or 3 for 3-arc second (lower detail)"))
-    except ValueError:
-        print("Please input either 1 or 3")
-        continue
-    if detail == 1 or detail == 3:
-        break
-    else:
-        print("Please enter either 1 or 3")
-while True:
-    try:
-        units = input("Select units of altitude. Enter 'Feet' or 'Meters'")
-    except ValueError:
-        print("please input either 'Feet' or 'Meters'")
-        continue
-    if units in ("Feet", "Meters"):
-        break
-    else:
-        print("please enter either 'Feet' or 'Meters'")
-
-# Define some parameters for later use based on input        
-if units == "Feet":
-    unitscale = 3.281 # This is feet per 1 meter
-else:
-    unitscale = 1
-csvtarget = np.zeros((int(3600/detail)**2,3)) # Either 3600X3600 or 1200X1200. One less than file dimension because files overlap. 
-width = int(3600/detail) + 1 # This is used for for both x and y dimensions. The files are square.
-horizontalscale = 30*detail # This corresponds to the granularity (in meters) of the data. Makes proportions right in Rhinoceros. 
+# Select target coordinates, level of detail, and units of altitude and define parameters based on input
+parameters = srtmchoose.srtmChoose()
+target = parameters.chooseTarget()
+detail, csvtarget, width, horizontalscale = parameters.chooseDetail()
+unitscale = parameters.chooseUnits()
 
 # Download, unzip, and parse HGT file
 downloader = srtmdownload.srtmDownloader()
@@ -54,9 +20,9 @@ else:
     downloader.downloadFileL3(target)
     parser.parseFileL3(target+".hgt")
     
-# Read data from file into numpy array and save as a CSV file. There are three columns and each row represents X, Y, and Z dimensions    
+# Read data from file into numpy array and save as a CSV file. There are three columns with X, Y, and Z dimensions for each point   
 i = 0
-for ydimension in range(1, width):
+for ydimension in range(1, width): # Technically height here, but it's a square
     for xdimension in range(1, width):
         csvtarget[i][0]=int(xdimension)*horizontalscale*unitscale
         csvtarget[i][1]=-int(ydimension)*horizontalscale*unitscale
