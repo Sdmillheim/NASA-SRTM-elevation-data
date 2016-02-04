e3c57092f7534510cdca1b7ad4b098d371f588f2
Stephen <Sdmillheim@users.noreply.github.com>
2016-02-04 15:27:09 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-import numpy as np
 import srtmchoose
 import srtmdownload
 import srtmparse
+import srtmwrite
 
 # Select target coordinates, level of detail, and units of altitude. 
 # Define parameters based on input
@@ -22,15 +22,7 @@ else:
     parser.parseFileL3(target+".hgt")
     
 # Read data from file into numpy array and save as a CSV file. There are three columns with X, Y, and Z dimensions for each point   
-i = 0
-for ydimension in range(1, width): # Technically height here, but it's a square
-    for xdimension in range(1, width):
-        csvtarget[i][0]=int(xdimension)*horizontalscale*unitscale
-        csvtarget[i][1]=-int(ydimension)*horizontalscale*unitscale
-        csvtarget[i][2]=int(parser.hgtcontents[width*ydimension+xdimension]*unitscale)
-        i += 1
-        
-csvfilename = input("What would you like to name your CSV file?")
-np.savetxt(csvfilename + ".csv", csvtarget, delimiter=",")
+writer = srtmwrite.srtmWriter()
+writer.srtmWriteFile(csvtarget, width, horizontalscale, hgtcontents, unitscale)
 
 # This csv file can be imported directly into Rhinoceros 5 software to create a 3D point cloud of the topography.
