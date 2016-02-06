2b2cb838163c9cd6cb5fee2f92a7c3cdbdaf5c1f
Stephen <Sdmillheim@users.noreply.github.com>
2016-02-06 11:34:51 -0800

    Update srtmwrite.py

--- srtmwrite.py
+++ srtmwrite.py
@@ -3,13 +3,14 @@
 import numpy as np
 
 class srtmWriter(object):
-    def srtmWriteFile(self, csvtarget, width, hgtcontents, unitscale):
+    def srtmWriteFile(self, hgtcontents, detail, unitscale):
+        csvtarget = np.zeros(((3600/detail)**2,3)) #either 3600X3600 or 1200X1200. One less than file dimension because files overlap. 
         i = 0
-        for ydimension in range(1, width): #technically 'height' in this loop, but it's a square
-            for xdimension in range(1, width):
-                csvtarget[i][0]=int(xdimension)*unitscale*detail*30
-                csvtarget[i][1]=-int(ydimension)*unitscale*detail*30
-                csvtarget[i][2]=int(hgtcontents[width*ydimension+xdimension]*unitscale)
+        for ydimension in range(1, int(3600/detail + 1)): # 1 to int(3600/detail + 1) represents length of x or y dimension of square
+            for xdimension in range(1, int(3600/detail + 1)): 
+                csvtarget[i][0]=int(xdimension)*unitscale*detail*30  # x dimension of 3D space
+                csvtarget[i][1]=-int(ydimension)*unitscale*detail*30 # y dimension of 3D space
+                csvtarget[i][2]=int(hgtcontents[width*ydimension+xdimension]*unitscale) # z dimension of 3D space
                 i += 1
         
         csvfilename = input("What would you like to name your CSV file?")
