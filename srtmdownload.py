9f1e3801b399aa2fcb1b2720a62a8a96e1cbbdc7
Stephen <Sdmillheim@users.noreply.github.com>
2016-02-06 12:11:55 -0800

    Update srtmdownload.py
    Edited for readability

--- srtmdownload.py
+++ srtmdownload.py
@@ -1,12 +1,14 @@
-# Defines a class with functions to download zipped file and extract contents as hgt file named 'target' + .hgt
+# Defines a class with functions to download zipped file and extract contents as hgt file named 'target'.hgt
 import urllib
 import zipfile
 
 class srtmDownloader(object):
     def downloadFileL1(self,target):
-        file = urllib.request.urlretrieve("http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL1.003/2000.02.11/"+target+".SRTMGL1.hgt.zip", "file.hgt.zip")
+        url = "http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL1.003/2000.02.11/" + target + ".SRTMGL1.hgt.zip"
+        file = urllib.request.urlretrieve(url, "file.hgt.zip")
         zipfile.ZipFile('file.hgt.zip').extractall()
     def downloadFileL3(self,target):
-        file = urllib.request.urlretrieve("http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL3.003/2000.02.11/"+target+".SRTMGL3.hgt.zip", "file.hgt.zip")
+        url = "http://e4ftl01.cr.usgs.gov/SRTM/SRTMGL3.003/2000.02.11/" + targe + ".SRTMGL3.hgt.zip"
+        file = urllib.request.urlretrieve(url, "file.hgt.zip")
         zipfile.ZipFile('file.hgt.zip').extractall()
         
